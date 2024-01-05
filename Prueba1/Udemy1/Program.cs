d5adb7cfea8a908083d18970d41b682fe684ae8d
Lopezadri <l18100145@nlaredoi.tecnm.mx>
2024-01-05 12:08:42 -0600

    Directorios

---
+++ Prueba1/Udemy1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Udemy1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string ruta = @"C:\Users\Lopezadri\Documents\Archivos\Maestros";
+
+            
+
+            List<string> list = Directory.EnumerateFiles(ruta).ToList();
+
+
+            foreach (string s in list)
+            {
+                Console.WriteLine(s);
+            }
+           
+            Console.ReadKey();
+           
+        }  
+    }
+}
